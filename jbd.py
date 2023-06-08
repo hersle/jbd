3a987f6edd7df221e0cf918395fd05e4d2702de6
Herman Sletmoen <hermansletmoen@gmail.com>
2023-06-08 11:56:00 +0200

    Don't re-run run simulations

--- jbd.py
+++ jbd.py
@@ -8,6 +8,8 @@ import numpy as np
 from scipy.stats import qmc
 from classy import Class
 
+COLAEXEC = os.path.expanduser("~/FML/FML/COLASolver/nbody")
+
 class ParameterSpace:
     def __init__(self, params):
         self.param_names = list(params.keys())
@@ -27,17 +29,20 @@ class Simulation:
         self.name = "testsim"
         self.directory = self.name + "/"
 
-        #self.prepare()
-        self.run_class(params)
-        self.run_cola(params)
+        # jump into simulation directory
+        os.makedirs(self.directory, exist_ok=True)
+        os.chdir(self.directory)
+
+        if not self.completed():
+            print(f"Simulating \"{self.name}\"")
+            self.run_class(params)
+            self.run_cola(params)
+
+        # jump out of simulation directory
+        os.chdir("..")
 
     def completed(self):
-        pass # TODO: check if already complete (and don't re-run, in that case)
-
-    def prepare(self):
-        if os.path.isdir(self.directory):
-            shutil.rmtree(self.directory)
-        os.makedirs(self.directory)
+        return os.path.isfile(f"pofk_{self.name}_cb_z0.000.txt")
 
     def write_data(self, filename, cols, colnames=None):
         if isinstance(cols, dict):
@@ -46,10 +51,10 @@ class Simulation:
             return self.write_data(filename, cols, colnames)
 
         header = None if colnames is None else " ".join(colnames)
-        np.savetxt(self.directory + filename, np.transpose(cols), header=header)
+        np.savetxt(filename, np.transpose(cols), header=header)
 
     def write_file(self, filename, string):
-        with open(self.directory + filename, "w") as file:
+        with open(filename, "w") as file:
             file.write(string)
 
     def run_class(self, params):
@@ -75,18 +80,16 @@ class Simulation:
         a1, a2 = 1/(z1+1), 1/(z2+1)
         as_ = 10 ** np.linspace(np.log10(a1), np.log10(a2), 100)
         zs  = 1/as_ - 1
-        zs  = np.round(zs, 3) # TODO: round for keeping filenames readable?
-        zs  = np.flip(zs) # TODO: ???
+        zs  = np.flip(zs) # TODO: from 0 ???
         nsnaps = len(zs)
 
         # for each redshift snapshot, write transfer function
         transfer_filenames = []
-        for i in range(0, nsnaps):
-            z = zs[i]
+        for i, z in enumerate(zs):
             transfer = self.cosmology.get_transfer(z=z, output_format="camb")
             while len(transfer) < 13:
                 transfer[f"dummy{len(transfer)}"] = np.zeros_like(list(transfer.values())[0])
-            transfer_filename = f"transfer{i}.txt"
+            transfer_filename = f"transfer_z{z:.3f}.txt"
             self.write_data(transfer_filename, transfer)
             transfer_filenames.append(transfer_filename)
 
@@ -110,26 +113,23 @@ class Simulation:
             "cosmology_Neffective": params["Neff"],
             "cosmology_TCMB_kelvin": params["Tγ0"],
 
-            "particle_Npart_1D": 320, # TODO: increase
+            "particle_Npart_1D": 64, # TODO: increase
 
             "timestep_nsteps": [30],
 
             "ic_random_seed": 1234,
             "ic_initial_redshift": z0,
-            "ic_nmesh" : 320,
+            "ic_nmesh" : 64,
             "ic_type_of_input": "transferinfofile",
             "ic_input_filename": "transfer.txt",
             "ic_input_redshift": 0.0, # TODO: ???
 
-            "force_nmesh": 320,
+            "force_nmesh": 64,
             "output_folder": ".",
             "output_redshifts": [0.0],
-            #"Ntimesteps": 30,
-            #"zini": 30.0,
-            #"kcola": False, # TODO
-            #"cosmology": "LCDM",
         }
 
+        # TODO: use some lua library for this?
         colafile = "cola_input.lua"
         contents = ""
         for param in params_cola:
@@ -144,8 +144,10 @@ class Simulation:
             contents += "\n"
         self.write_file(colafile, contents)
 
-        colaexec = os.path.expanduser("~/FML/FML/COLASolver/nbody")
-        proc = subprocess.run([colaexec, colafile], cwd=self.directory)
+        cmd = [COLAEXEC, colafile]
+        with open("cola_log.txt", "w") as colalog:
+            proc = subprocess.run(cmd, stdout=colalog, stderr=colalog)
+        print()
 
 #if __name__ == "__main__":
 params0 = {
